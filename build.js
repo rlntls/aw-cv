03d9f3c8fda88a9a846dc517a919562c0bdbdfb9
Alex W <awastell@gmail.com>
2026-03-07 22:12:38 +0000

    Update CV

--- build.js
+++ build.js
@@ -7,6 +7,15 @@ const month = date.toLocaleString('default', { month: 'short' });
 const year = date.getFullYear();
 const filename = `Alex-Wastell_CV_${month}-${year}.pdf`;
 
+// Delete old PDFs in root directory
+const files = fs.readdirSync(__dirname);
+for (const file of files) {
+  if (file.endsWith('.pdf')) {
+    fs.unlinkSync(path.join(__dirname, file));
+    console.log(`Removed old PDF: ${file}`);
+  }
+}
+
 (async () => {
     try {
         const pdf = await mdToPdf({ path: 'resume.md' }, {
