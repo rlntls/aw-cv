b7e3e141ea48432c7717803eda1bced3d2f83399
Alex W <awastell@gmail.com>
2026-03-07 23:43:52 +0000

    Update CV

--- build.js
+++ build.js
@@ -26,6 +26,9 @@ for (const file of files) {
 <html>
 <head>
     <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;600;700&display=swap" rel="stylesheet">
     <style>${fs.readFileSync('style.css', 'utf8')}</style>
     <script src="file://${__dirname}/node_modules/pagedjs/dist/paged.polyfill.js"></script>
 </head>
@@ -36,17 +39,34 @@ for (const file of files) {
 
         const browser = await puppeteer.launch({
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            args: [
+                '--no-sandbox', 
+                '--disable-setuid-sandbox',
+                '--disable-web-security',
+                '--font-render-hinting=none' // Can sometimes help with font overhead
+            ]
         });
         
         const page = await browser.newPage();
+        
+        // Block unnecessary resources to keep environment clean
+        await page.setRequestInterception(true);
+        page.on('request', (request) => {
+            if (['image', 'media', 'font'].includes(request.resourceType()) && !request.url().includes('fonts.googleapis.com') && !request.url().includes('fonts.gstatic.com')) {
+                request.abort();
+            } else {
+                request.continue();
+            }
+        });
+
         await page.setContent(template, { waitUntil: 'networkidle0' });
 
         await page.pdf({
             path: filename,
             format: 'A4',
-            printBackground: true,
-            preferCSSPageSize: true
+            printBackground: false, // Minimise overhead as requested
+            preferCSSPageSize: true,
+            displayHeaderFooter: false, // Ensure no metadata headers are added
         });
 
         await browser.close();
